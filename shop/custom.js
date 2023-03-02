6ce0ed65feb743c1352d6919c139ba37be9df55d
Frontend JavaScript <frontendjavascript@C3-23.local>
2023-03-02 20:51:47 +0200

    curs2

--- shop/custom.js
+++ shop/custom.js
@@ -1,17 +1,3 @@
-
-// document.addEventListener("DOMContentLoaded", function(){
-//     var myScrollSpy = new bootstrap.ScrollSpy(document.body, {
-//         target: "#myNavbar"
-//     })
-// });
-
-$(document).ready(function () {
-    $("body").scrollspy({
-        target: "#navbar_top"
-    })
-
-});
-
 const scrollBtn = document.querySelector(".gototop");
 const btnVisibility = () => {
 if (window.scrollY > 400) {
